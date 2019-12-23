7262cf0b2b243f1cb4716990305aeedc625d0fe7
Jehyeuk <jhyoo@vetec.co.kr>
2019-12-23 10:23:32 +0900

    target_temp_test

--- model/db_sql.js
+++ model/db_sql.js
@@ -1,17 +1,20 @@
 var pool = require('./db_connect');
 
+
 module.exports = function(){
-    return {
+     return {
         select : function(callback){
             pool.getConnection(function(err,con){
                 var sql = 'select * from bwanalogtable where tagname="temp"';
                 con.query(sql,function(err,result,fields){
                     con.release();
+
                     if(err) return callback(err);
+
                     callback(null,result);
                 });
             });
         },
         pool : pool
     }
-};
+ }();
