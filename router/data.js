74011d024e8a9a1789f93241795f6e0d6b07fc96
Jehyeuk <jhyoo@vetec.co.kr>
2019-12-24 16:24:18 +0900

    page_complete

--- router/data.js
+++ router/data.js
@@ -6,7 +6,12 @@ var pool = require('../model/db_connect');
 
 router.get('/data_print', async function (req, res) {
     var par=[];
+    console.log("current_page:"+req.query.current_page);
 
+    let rowPerPage = 10;    // 페이지당 보여줄 글목록 : 10개
+    let currentPage = req.query.current_page;  
+    let beginRow =(currentPage-1)*rowPerPage;
+    let model = {};
     if(req.query.date===undefined){
         var sql = 'select * from bwanalogtable where tagname="temp" ORDER BY LogDate DESC, LogTime DESC LIMIT 100';
     }else{
@@ -17,12 +22,30 @@ router.get('/data_print', async function (req, res) {
     
     data = await con.query(sql,par);
 
+    lastPage = data.length / rowPerPage; 
+    if(data.length % rowPerPage != 0){ 
+        lastPage++;
+    }
+
+    if(req.query.date===undefined){
+        var sql = 'select * from bwanalogtable where tagname="temp" ORDER BY LogDate DESC, LogTime DESC LIMIT ?,?';
+        var par=[beginRow,rowPerPage];
+    }else{
+        var sql = 'select * from bwanalogtable where tagname="temp" and date(logdate)= ? ORDER BY LogDate DESC, LogTime DESC LIMIT ?,?';
+        var par=[req.query.date,beginRow,rowPerPage];
+    }
+
+    data = await con.query(sql,par);
+    
+    model.list = data;
+    model.currentPage=currentPage;
+    model.lastPage = lastPage;
 
     sql ='select target_temp from target_temp where id =?';
     target_temp = await con.query(sql,["test_user"]);
 
     con.release();
-    res.render('data',{data:data,target_temp:target_temp});
+    res.render('data',{model:model,target_temp:target_temp});
   
   });
 module.exports = router;
